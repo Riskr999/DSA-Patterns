b75324f88c7ff30f1cd1655f542f6f8cca5c6127
Riskr999 <fbax941@gmail.com>
2026-06-16 09:42:09 +0530

    Create Implement_Stack_using_Queues.java

---
+++ Stack Patterns/Implementation/Implement_Stack_using_Queues.java
@@ -0,0 +1,28 @@
+class MyStack {
+    private Queue<Integer> queue;
+    
+
+    public MyStack() {
+        queue = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        int size = queue.size();
+        queue.add(x);
+        for(int i = 0; i<size;i++){
+                queue.add(queue.poll());
+        }
+    }
+    
+    public int pop() {
+        return queue.poll();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
