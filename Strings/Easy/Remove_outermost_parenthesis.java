b2649aa20fdeb0b7c87927f1991f9af943fa581a
Riskr999 <fbax941@gmail.com>
2026-06-29 01:35:26 +0530

    Create Remove_outermost_parenthesis.java

---
+++ Strings/Easy/Remove_outermost_parenthesis.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '('){
+                if(!stack.isEmpty()){
+                    sb.append(ch);
+                }
+                stack.push(ch);
+            }
+            if(ch == ')'){
+                
+                stack.pop();
+                if(!stack.isEmpty()){
+                    sb.append(ch);
+                }
+            }
+            
+
+        }
+        
+        
+
+        return sb.toString();
+
+        
+    }
+}
