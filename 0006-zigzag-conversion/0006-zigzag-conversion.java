fe1feb476e82ece3038fb68f0afb75e6fb01dc64
Riskr999 <fbax941@gmail.com>
2026-09-18 04:09:24 +0530

    Time: 4 ms (88.06%), Space: 46.5 MB (76.85%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,30 +1,31 @@
 class Solution {
     public String convert(String s, int numRows) {
-        int n = s.length();
-        if(numRows==1||s.length()<=numRows) return s;
-
-        StringBuilder[] sb = new StringBuilder[numRows];
-
-        for(int i =0;i<numRows;i++){
-            sb[i] = new StringBuilder(); 
+        if(numRows==1||s.length()<numRows){
+            return s;
         }
-        int currRow = 0;
+        
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for(int i =0;i<numRows;i++){
+            rows[i] = new StringBuilder();
+        }
+
+        int currRows = 0;
         int direction = -1;
 
-        for(char c : s.toCharArray()){
-            sb[currRow].append(c);
-            if (currRow == 0 || currRow == numRows - 1) {
-                direction = -direction; // Flip the direction
+        for(char c: s.toCharArray()){
+            rows[currRows].append(c);
+
+            if(currRows==0||currRows==numRows-1){
+                direction =-direction;
             }
-            currRow += direction;
+            currRows += direction;
         }
+
         StringBuilder result = new StringBuilder();
-        for (StringBuilder row :sb) {
+        for(StringBuilder row:rows){
             result.append(row);
         }
-
-        return result.toString();
-
+        return   result.toString();
         
         
     }
